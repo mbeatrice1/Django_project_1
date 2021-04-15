79b54f208b3ce0da37a9283fc4f84b83fa4ef8e8
mbeatrice1 <mahorobeatrice9@gmail.com>
2021-04-15 20:19:55 +0200

    added templates

--- galleryApp/settings.py
+++ galleryApp/settings.py
@@ -31,9 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'galleria'
-    'bootstrap3'
+    
     'galleria.apps.GalleriaConfig',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
