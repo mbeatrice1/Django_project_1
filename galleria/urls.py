9f6bd47beb711115f218f91559552ed6a6830e4f
mbeatrice1 <mahorobeatrice9@gmail.com>
2021-04-15 21:54:51 +0200

    modified the view

--- galleria/urls.py
+++ galleria/urls.py
@@ -1,8 +1,16 @@
-from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+
+
 
 urlpatterns=[
-
-url('^$',views.nav,name = 'welcome'),
+    url(r'^$', views.gallery, name = 'gallery'),
+    url(r'^search/', views.search_results, name = 'search_results'),
+    url(r'^categories/', views.display_images_categories, name = 'categories'),
+    url(r'^locations/', views.display_images_locations, name = 'locations'),
 
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
