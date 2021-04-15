dc17ebebb8d4ff44f19c16b0f73cde6118a66b7e
mbeatrice1 <mahorobeatrice9@gmail.com>
2021-04-15 09:40:56 +0200

    added bootstrap3

--- galleryApp/settings.py
+++ galleryApp/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'galleria'
+    'bootstrap3'
+    'galleria.apps.GalleriaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
